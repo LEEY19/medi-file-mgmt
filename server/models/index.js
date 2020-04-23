fb523affa03445c125aee4faf9d512a47cff37e2
Yong En Lee <yongenlee@Yongs-MacBook-Pro-2.local>
2020-04-23 22:28:55 +0800

    created db config

---
+++ server/models/index.js
@@ -0,0 +1,26 @@
+const env = require("../config/db.config.js");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(env.DB, env.USER, env.PASSWORD, {
+  host: env.HOST,
+  dialect: env.dialect,
+  operatorsAliases: false,
+
+  pool: {
+    max: env.pool.max,
+    min: env.pool.min,
+    acquire: env.pool.acquire,
+    idle: env.pool.idle
+  }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.users = require("./user.model.js")(sequelize, Sequelize);
+db.roles = require("./role.model.js")(sequelize, Sequelize);
+db.files = require("./file.model.js")(sequelize, Sequelize);
+
+module.exports = db;
